a46849cd0b0adcde591b7fc883ac7a4f370b369b
CORPSETARUN <113999899+CORPSETARUN@users.noreply.github.com>
2023-02-12 16:21:51 +0530

    first commit

---
+++ tik/script.js
@@ -0,0 +1,153 @@
+const statusDisplay = document.querySelector('.game--status');
+
+let gameActive = true;
+let currentPlayer = "X";
+let gameState = ["", "", "", "", "", "", "", "", ""];
+
+const winningMessage = () => `Player ${currentPlayer} has won!`;
+const drawMessage = () => `Game ended in a draw!`;
+const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
+
+statusDisplay.innerHTML = currentPlayerTurn();
+
+const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+function handleCellPlayed(clickedCell, clickedCellIndex) {
+    gameState[clickedCellIndex] = currentPlayer;
+    clickedCell.innerHTML = currentPlayer;
+}
+
+function handlePlayerChange() {
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+    statusDisplay.innerHTML = currentPlayerTurn();
+}
+
+function handleResultValidation() {
+    let roundWon = false;
+    for (let i = 0; i <= 7; i++) {
+        const winCondition = winningConditions[i];
+        let a = gameState[winCondition[0]];
+        let b = gameState[winCondition[1]];
+        let c = gameState[winCondition[2]];
+        if (a === '' || b === '' || c === '') {
+            continue;
+        }
+        if (a === b && b === c) {
+            roundWon = true;
+            break
+        }
+    }
+
+    if (roundWon) {
+        statusDisplay.innerHTML = winningMessage();
+        gameActive = false;
+        return;
+    }
+
+    let roundDraw = !gameState.includes("");
+    if (roundDraw) {
+        statusDisplay.innerHTML = drawMessage();
+        gameActive = false;
+        return;
+    }
+
+    handlePlayerChange();
+}
+
+function handleCellClick(clickedCellEvent) {
+    const clickedCell = clickedCellEvent.target;
+    const clickedCellIndex = parseInt(clickedCell.getAttribute('data-cell-index'));
+
+    if (gameState[clickedCellIndex] !== "" || !gameActive) {
+        return;
+    }
+
+    handleCellPlayed(clickedCell, clickedCellIndex);
+    handleResultValidation();
+}
+
+function handleRestartGame() {
+    gameActive = true;
+    currentPlayer = "X";
+    gameState = ["", "", "", "", "", "", "", "", ""];
+    statusDisplay.innerHTML = currentPlayerTurn();
+    document.querySelectorAll('.cell').forEach(cell => cell.innerHTML = "");
+}
+
+
+document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleCellClick));
+document.querySelector('.game--restart').addEventListener('click', handleRestartGame);
+
+const cells = document.querySelectorAll(".cell");
+const resetButton = document.querySelector("#resetButton");
+const player1NameInput = document.querySelector("#player1Name");
+const player2NameInput = document.querySelector("#player2Name");
+let player1Name = "Player 1";
+let player2Name = "Player 2";
+
+for (let i = 0; i < cells.length; i++) {
+  cells[i].addEventListener("click", cellClicked);
+}
+
+resetButton.addEventListener("click", resetGame);
+
+function cellClicked(e) {
+  if (e.target.innerHTML === "") {
+    e.target.innerHTML = currentPlayer;
+
+    if (checkForWinner()) {
+      alert(`${currentPlayer === "X" ? player1Name : player2Name} wins!`);
+      return;
+    }
+
+    if (currentPlayer === "X") {
+      currentPlayer = "O";
+    } else {
+      currentPlayer = "X";
+    }
+  }
+}
+
+function checkForWinner() {
+  const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < winningCombinations.length; i++) {
+    const [a, b, c] = winningCombinations[i];
+    if (
+      cells[a].innerHTML === currentPlayer &&
+      cells[b].innerHTML === currentPlayer &&
+      cells[c].innerHTML === currentPlayer
+    ) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+function resetGame() {
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].innerHTML = "";
+  }
+  currentPlayer = "X";
+  player1Name = player1NameInput.value || "Player 1";
+  player2Name = player2NameInput.value || "Player 2";
+}
